34287bc4b657de8dab64e2651961938d4bc6c00b
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-06-16 03:45:46 +0900

    プロパティにした

--- Common/Player.cs
+++ Common/Player.cs
@@ -13,24 +13,24 @@ namespace MagicOnionCommon
         /// 名前
         /// </summary>
         [Key(0)]
-        public string Name = "";
+        public string Name { get; set; } = "";
 
         /// <summary>
         /// X座標
         /// </summary>
         [Key(1)]
-        public float X = 0.0f;
+        public float X { get; set; } = 0.0f;
 
         /// <summary>
         /// Y座標
         /// </summary>
         [Key(2)]
-        public float Y = 0.0f;
+        public float Y { get; set; } = 0.0f;
 
         /// <summary>
         /// Z座標
         /// </summary>
         [Key(3)]
-        public float Z = 0.0f;
+        public float Z { get; set; } = 0.0f;
     }
 }
