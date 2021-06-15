e9124af3f87805b92e648c1a3fbf13e7e15afc4e
YanaPIIDXer <>
2021-06-16 02:37:58 +0900

    VSCodeのタスクはWindows向けなので、同じようなビルドプロセスをBootServer.shにブチ込んだ

--- BootServer.sh
+++ BootServer.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+dotnet build Common/MagicOnionCommon.csproj
+cp Common/bin/Debug/netstandard2.0/MagicOnionCommon.dll Client/MagicOnionTest/Assets/Binary
 dotnet publish Server/MagicOnionServer.csproj -r osx.10.11-x64
 Server/bin/Debug/netcoreapp3.1/osx.10.11-x64/publish/MagicOnionServer
