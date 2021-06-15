8aef48d5fa3568b52c537bc21eaab79f9fa4e970
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-06-16 03:42:48 +0900

    プレイヤーの情報クラス

--- Common/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MagicOnionCommon
-{
-    public class Class1
-    {
-    }
-}
---
+++ Common/Player.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using MessagePack;
+
+namespace MagicOnionCommon
+{
+    /// <summary>
+    /// プレイヤーの情報
+    /// </summary>
+    [MessagePackObject]
+    public class Player
+    {
+        /// <summary>
+        /// 名前
+        /// </summary>
+        [Key(0)]
+        public string Name = "";
+
+        /// <summary>
+        /// X座標
+        /// </summary>
+        [Key(1)]
+        public float X = 0.0f;
+
+        /// <summary>
+        /// Y座標
+        /// </summary>
+        [Key(2)]
+        public float Y = 0.0f;
+
+        /// <summary>
+        /// Z座標
+        /// </summary>
+        [Key(3)]
+        public float Z = 0.0f;
+    }
+}
