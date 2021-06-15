900e58f2b9daf0869aaf8e101fdb667e62f7b621
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-06-16 01:16:03 +0900

    サーバ側のプロジェクト作成

---
+++ Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MagicOnionServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
